2b10e4a98a5ae28943a3c9e9306b825828520b41
kainat <kainatfaisal50@gmail.com>
2024-09-11 04:25:11 +0500

    sss

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
-const TeamChannelPreview = ({channel, type}) => {
+const TeamChannelPreview = ({setActiveChannel, setIsCreating, setIsEditing, setToggleContainer, channel, type}) => {
     const { channel: activeChannel, client } = useChatContext();
     const ChannelPreview = () => (
        <p className="channel-preview__item">
